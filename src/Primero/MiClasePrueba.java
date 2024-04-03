606bae464a1563623536c171337d128de62976c5
michelle <michelle@localhost>
2024-04-03 10:50:41 +0200

    commit tres

--- src/Primero/MiClasePrueba.java
+++ src/Primero/MiClasePrueba.java
@@ -5,7 +5,8 @@ public class MiClasePrueba {
 	public static void main(String[] args) {
 		System.out.println("Michelle");
 		System.out.println("Pruebita");
-
+		System.out.println("Pruebis");
+		System.out.println("queeee????");
 	}
 
 }
